2de9c3d4ca4f32260b7595841092a517854161ee
Djinnet renshira <lukas.alexander@hotmail.com>
2021-08-19 13:50:42 +0200

    Add project files.

---
+++ SqliteDemo.Library/DAL/ApplicationDBContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace SqliteDemo
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public DbSet<Depts> Dept { get; set; }
+
+        public ApplicationDBContext()
+            :base()
+        {
+
+        }
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
---
+++ SqliteDemo.Library/Models/Depts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SqliteDemo
+{
+    [Table("Dept")]
+    public class Depts
+    {
+        [Key]
+        [Required]
+        public int deptid { get; set; }
+
+        [StringLength(50)]
+        public string dname { get; set; }
+
+        [StringLength(50)]
+        public string location { get; set; }
+    }
+}
